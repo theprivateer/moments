e464932744003a299bbc48e125647d09a8ca6d91
Phil Stephens <hello@philstephens.com>
2026-03-13 16:51:26 +1000

    Add auto-expanding textarea to create and edit moment forms

--- resources/views/livewire/edit-moment.blade.php
+++ resources/views/livewire/edit-moment.blade.php
@@ -17,8 +17,13 @@
             </label>
             <textarea
                 id="body"
+                x-data="{
+                    init() { this.resize(); this.$watch('$wire.body', () => this.resize()); },
+                    resize() { this.$el.style.height = 'auto'; this.$el.style.height = this.$el.scrollHeight + 'px'; }
+                }"
+                @input="resize()"
                 wire:model="body"
-                rows="6"
+                style="min-height: 8rem"
                 class="w-full border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-hidden focus:ring-2 focus:ring-gray-400 resize-none"
             ></textarea>
             @error('body')
