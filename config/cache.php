ee27a63223ca9293617ce68cb1aa51b3ff7ea248
Phil Stephens <hello@philstephens.com>
2026-03-24 19:56:43 +1000

    Upgrade to Laravel 13 with updated framework, tinker, Symfony 8, and config changes

--- config/cache.php
+++ config/cache.php
@@ -114,4 +114,17 @@ return [
 
     'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Serializable Classes
+    |--------------------------------------------------------------------------
+    |
+    | This option controls which classes may be unserialized when retrieving
+    | cached data. Setting this to false prevents PHP object unserialization
+    | to protect against deserialization attacks if your APP_KEY is leaked.
+    |
+    */
+
+    'serializable_classes' => false,
+
 ];
